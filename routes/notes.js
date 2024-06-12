b8a6458b8005291aae2cf4a630e454a335ae5375
Yuki Oka <yuki@YukinoMacBook-Air.local>
2024-06-13 02:20:41 +0900

    add notes page mongodb

--- routes/notes.js
+++ routes/notes.js
@@ -1,30 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-// レスポンスのデータ(ノート全体)
-const responseObjectDataAll = {
-	textObject1 : {
-	id: 1,
-	title: 'ノート１のタイトルです',
-	subTitle: 'ノート１のタイトルです',
-	bodyText: 'ノート１の本文です',
-	},
-	textObject2 : {
-		id: 2,
-		title: 'ノート２のタイトルです',
-		subTitle: 'ノート２のタイトルです',
-		bodyText: 'ノート２の本文です',
-		},
-};
-/**
- * メモを全件取得するAPI
- * @returns {Object[]} data
- * @returns {number} data.id - ID
- * @returns {string} data.title - タイトル
- * @returns {string} data.text - 内容
- */
-router.get('/', function(req, res, next) {
-  res.json(responseObjectDataAll);
-});
-
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri ="mongodb+srv://cu2101110130:v8mQM8f%40c@cluster0.d7hlzfq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
+// idが1のドキュメントを取得
+const query = { id: 2 };
+const note = await notes.findOne(query);
+res.json(note); })
+module.exports = router;
 module.exports = router;
